a7b0c461541db20e3a88c085b19a9735ecfb3993
Arran Walker <arran.walker@fiveturns.org>
2020-10-01 22:55:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y fio
-export $TEST_DIR=/mnt
+export TEST_DIR=/mnt
 mkdir -p $TEST_DIR
 df -h
 fio --name=write_throughput --directory=$TEST_DIR --numjobs=2 --size=2G --time_based --runtime=60s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1
