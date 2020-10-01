745f5df25e20b7815aa72360f1e90a57e215596f
Arran Walker <arran.walker@fiveturns.org>
2020-10-02 00:53:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 apt-get update
-apt-get install -y fio
+apt-get install -y fio sysbench
 export TEST_DIR=/mnt
 mkdir -p $TEST_DIR
 df -h
+sysbench cpu --threads=2 run
 fio --name=write_throughput --directory=$TEST_DIR --numjobs=2 --size=2G --time_based --runtime=60s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1
 fio --name=write_iops --directory=$TEST_DIR --size=2G --time_based --runtime=60s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=4K --iodepth=256 --rw=randwrite --group_reporting=1
 fio --name=read_throughput --directory=$TEST_DIR --numjobs=2 --size=2G --time_based --runtime=60s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=read --group_reporting=1
